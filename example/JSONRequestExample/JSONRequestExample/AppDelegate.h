4c7e5bd15f71161ca45ae5bc658f12b1c50d2cdd
stcui <saintcui@gmail.com>
2012-05-02 18:18:29 +0800

    add jsonkit submodule; add example project

---
+++ example/JSONRequestExample/JSONRequestExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JSONRequestExample
+//
+//  Created by stcui on 12-5-2.
+//  Copyright (c) 2012年 stcui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ example/JSONRequestExample/JSONRequestExample/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  JSONRequestExample
+//
+//  Created by stcui on 12-5-2.
+//  Copyright (c) 2012年 stcui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
